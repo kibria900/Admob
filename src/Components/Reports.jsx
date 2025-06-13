9dadeddcdd85110834537e1f000643a0f5421e71
kibria900 <kibriahassan3500@gmail.com>
2025-06-13 21:41:57 +0600

    testupdate

--- src/Components/Reports.jsx
+++ src/Components/Reports.jsx
@@ -4,8 +4,8 @@ import NavBar from './NavBar'
 const Reports = () => {
   return (
    <>
-  
-   <h1>jfjgjffkcvnkdfnk</h1>
+   <h1> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos sequi quia, dolores fuga magnam quibusdam, temporibus, animi ipsa nam eligendi aut veritatis iusto quas laboriosam. Quae molestias eveniet debitis explicabo.
+   </h1>
    </>
   )
 }
